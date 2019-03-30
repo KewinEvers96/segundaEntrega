f290f632d4968cc83489950a6800ba09e1568a11
Juan Misas <juanfer.misas@outlook.com>
2019-03-29 23:12:10 -0500

    da

--- src/app.js
+++ src/app.js
@@ -5,52 +5,6 @@ const hbs = require('hbs');
 const funciones = require('./funciones');
 const fs = require('fs');
 
-
-const listar = () => {
-    try{// Por si existe el archivo
-        listaUsuarios = require('./usuarios.json');
-    }
-    catch(error){ // Si el archivo no existe
-        listaUsuarios = [];
-    }
-};
-
-
-const registrar = (_nombre, _id, _correo, _telefono) => {
-    listar();// Llama la lista de documentos
-    let usuario= {
-        id:_id,
-        nombre: _nombre,
-        correo:_correo,
-        telefono:_telefono,
-        tipoUsuario:"aspirante"
-    }
-    let duplicado = listaUsuarios.find(buscar => buscar.id == usuario.nombre);
-    if(!duplicado){// si el usuario no está duplicado
-        listaUsuarios.push(usuario);
-        guardar("./src/usuarios.json");
-    }
-    else{
-        console.log("Error usuario duplicado");
-    }
-}
-
-
-const guardar = (nombreArchivo) => {
-    let datos = JSON.stringify(listaUsuarios);
-    fs.writeFile(nombreArchivo, datos, (err)=>{
-        if (err) throw (err);
-        console.log('Archivo guardado')
-    });
-}
-
-
-module.exports = {
-    listar, 
-    registrar,
-    guardar
-}
-
 //const directioriopublico = path.join(__dirname, '../public/img');
 //app.use(express.static(directioriopublico));
 
@@ -67,7 +21,7 @@ app.get('/registered', function (req, res) {
 	console.log(id)
 	console.log(nombre)
 
-	registrar(nombre, id, correo, telefono);
+	funciones.registrar(nombre, id, correo, telefono);
   	res.render('login.hbs')
 });
 
