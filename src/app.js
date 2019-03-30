68cf4f996664c25a1a444a6269e8c51d86b61e5b
Juan Misas <juanfer.misas@outlook.com>
2019-03-29 19:43:49 -0500

    dw
    Co-Authored-By: pancraci0 <pancraci0@users.noreply.github.com>

--- src/app.js
+++ src/app.js
@@ -2,6 +2,8 @@ const express = require('express');
 const app = express();
 const path = require('path');
 
+const directioriopublico = path.join(__dirname, '../public');
+app.use(express.static(directioriopublico));
 
 console.log(__dirname)
 
