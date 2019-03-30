79e102915739662290b2e46947d797e8a7b81302
KewinEvers96 <keversy@unal.edu.co>
2019-03-30 01:30:47 -0500

    coordinadorCursos

--- src/helpers.js
+++ src/helpers.js
@@ -49,3 +49,32 @@ hbs.registerHelper('listarDisponibles', () =>{
 
     return tablaT;
 });
+
+
+hbs.registerHelper('listarCursos', () =>{
+    listaCursos = require('./cursos.json');
+    tablaT = "<table border=1><thead>\
+    <th>Código</th>\
+    <th>Nombre curso</th>\
+    <th>Descripción</th>\
+    <th>Valor</th>\
+    <th>Estado</th>\
+    <th>Modalidad</th>\
+    <th>Intensidad horaria</th>\
+    </thead><tbody>";
+
+    listaCursos.forEach(curso => {
+       tablaT = tablaT +  "<tr><td>" + curso.idCurso +
+        "</td><td>" + curso.nombre + 
+        "</td><td>" + curso.descripcion + 
+        "</td><td>" + curso.valor + 
+        "</td><td>" + curso.estado + 
+        "</td><td>" + curso.modalidad + 
+        "</td><td>" + curso.intensidad+
+        "</td></tr>";
+    });
+
+    tablaT = tablaT + "</tbody><table>";
+
+    return tablaT;
+});
