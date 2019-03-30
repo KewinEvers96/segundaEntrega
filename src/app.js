9489ccfc8887cac8247763155ece5b2b0a7d9a3f
Juan Misas <juanfer.misas@outlook.com>
2019-03-30 00:10:12 -0500

    lk

--- src/app.js
+++ src/app.js
@@ -8,10 +8,65 @@ const fs = require('fs');
 //const directioriopublico = path.join(__dirname, '../public/img');
 //app.use(express.static(directioriopublico));
 
-
-
 app.set('view engine', 'hbs');
 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+app.get('/coordinador/index', function (req, res) {
+	id = parseInt(req.query.document);
+	console.log('index aspirante');
+  	res.render('usuario/aspirante/index.hbs',{id:idingreso})
+});
+
+app.get('/aspirante/index', function (req, res) {
+	id = parseInt(req.query.document);
+	console.log('index aspirante');
+  	res.render('usuario/aspirante/index.hbs',{id:idingreso})
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//////////////////////////////////////////////////////////////////////////////////
 
 app.get('/registered', function (req, res) {
 	nombre = req.query.name;
@@ -23,18 +78,33 @@ app.get('/registered', function (req, res) {
   	res.render('login.hbs')
 });
 
-
-
-
-
-
-app.get('/index', function (req, res) {
-	
-  	res.render('login.hbs')
-});
-
 app.get('/register', function (req, res) {
   res.render('usuario/registrar.hbs')
+});
+
+app.get('/requestLogin', function (req, res) {
+	id = parseInt(req.query.document);
+	listaUsuarios = require('./usuarios.json');
+    let usuario = listaUsuarios.find(buscar => buscar.id == id);
+    if(!usuario){
+    	console.log('usuario no encontrado');
+        res.render('login.hbs')    
+    }else{
+        // el usuario está
+        switch(usuario.tipoUsuario){
+            case "aspirante":
+            	console.log('ingreso aspirante');
+            	idingreso = id;
+            	res.render('usuario/aspirante/index.hbs',{id:idingreso})
+            break;
+            case "coordinador":
+            	console.log('ingreso coordinador');
+            	idingreso = id;
+            	res.render('usuario/coordinador/index.hbs',{id:idingreso})
+            // Codigo
+            break;
+        }
+    }
 });
 
 app.get('/login', function (req, res) {
