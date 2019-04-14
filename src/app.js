1f528f008cc45ccba34d98bf37111def4ddbb55c
Juan Misas <juanfer.misas@outlook.com>
2019-04-13 23:59:56 -0500

    j

--- src/app.js
+++ src/app.js
@@ -18,33 +18,6 @@ app.set('view engine', 'hbs');
 require('./helpers');
 
 
-////////////////////////////////////////////////////////////////////////////////////////////
-
-
-const MongoClient = require('mongodb').MongoClient;
-const assert = require('assert');
-
-// Connection URL
-const url = 'mongodb://localhost:27017';
-
-// Database Name
-const dbName = 'myproject';
-
-// Create a new MongoClient
-const client = new MongoClient(url, { useNewUrlParser: true });
-
-// Use connect method to connect to the Server
-client.connect(function(err) {
-  if (err) {
-    console.log("No se ha podido conectar");
-  }
-  assert.equal(null, err);
-  console.log("Conectado");
-
-  const db = client.db(dbName);
-
-  client.close();
-});
 
 
 
