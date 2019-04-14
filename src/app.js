f625fdc58c1d2aa0b0dc50ffcaae75faa2701fa7
Juan Misas <juanfer.misas@outlook.com>
2019-04-13 19:54:43 -0500

    dd

--- src/app.js
+++ src/app.js
@@ -35,8 +35,11 @@ const client = new MongoClient(url, { useNewUrlParser: true });
 
 // Use connect method to connect to the Server
 client.connect(function(err) {
+  if (err) {
+    console.log("No se ha podido conectar");
+  }
   assert.equal(null, err);
-  console.log("Connected successfully to server");
+  console.log("Conectado");
 
   const db = client.db(dbName);
 
